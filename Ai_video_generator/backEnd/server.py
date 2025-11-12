fb24207c63f1ca18e409b00bf16a16a0abf2ffe0
SteffanBurnette <burnettesteffanep7@gmail.com>
2025-11-12 18:57:34 -0500

    Text to video endpoint

---
+++ Ai_video_generator/backEnd/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, Blueprint
+from flask_cors import CORS
+import sys
+sys.path.insert(0, "../Ai_video_generator/backEnd/videoGenblueprint.py") 
+from videoGenblueprint import videoGen_bp
+
+app = Flask(__name__)
+CORS(app, support_credentials = True, origins = ["http://localhost3000"])
+
+
+#Blueprint endpoints
+app.register_blueprint(videoGen_bp, url_prefix = "/videoGen")
+
+@app.route("/")
+def basics():
+    return jsonify({"message": "Flask is working!"})
+
+#Basicx flask endpoint structure
+@app.route("/api/data")
+def get_data():
+    data = {"key": "value"}
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ Ai_video_generator/backEnd/videoGenblueprint.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, Flask, request, jsonify
+import time
+from google import genai
+from google.genai import types
+import os
+from dotenv import load_dotenv
+
+
+#Name of the blueprint
+videoGen_bp = Blueprint("video_generation", __name__)
+
+#Getting the neccessary credentials to use the model
+load_dotenv("C:/Users/Steffan/Desktop/Ai_advertisement_generator/Ai-Advertisement-Generator/Ai_video_generator/.env")
+API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key = API_KEY)
+
+#The endpoints
+
+#Takes a prompt and video file name to generate a video and save it with said name
+@videoGen_bp.route("/generate_video", methods = {"POST"})
+def generate_video():
+    data = request.get_json()
+    #Generates the video based on the users prompt
+    operation = client.models.generate_videos(
+    model="veo-3.1-generate-preview",
+    prompt=data.get("prompt"),
+    )
+
+    # Poll the operation status until the video is ready.
+    while not operation.done:
+        print("Waiting for video generation to complete...")
+        time.sleep(10)
+        operation = client.operations.get(operation)
+
+    # Download the generated video.
+    generated_video = operation.response.generated_videos[0]
+    client.files.download(file=generated_video.video)
+    generated_video.video.save(f"{data.get("name")}.mp4")
+    print(f"Generated video saved to {data.get("name")}.mp4")
+
+    return jsonify({"Message": f"The video {data.get("name")}.mp4 was successfully created"}), 200
+
+
+
