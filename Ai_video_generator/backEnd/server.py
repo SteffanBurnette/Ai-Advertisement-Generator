1b8407705caa9fb80c54a9f51a8303c814862f3c
SteffanBurnette <burnettesteffansep7@gmail.com>
2025-11-17 16:24:40 -0500

    Updated server file

--- Ai_video_generator/backEnd/server.py
+++ Ai_video_generator/backEnd/server.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, Blueprint
 from flask_cors import CORS
+from pymongo import MongoClient
+import gridfs
 import sys
 sys.path.insert(0, "../Ai_video_generator/backEnd/videoGenblueprint.py") 
 from videoGenblueprint import videoGen_bp
+from fetchVideoBP import fetchVideo
 
 app = Flask(__name__)
 CORS(app, support_credentials = True, origins = ["http://localhost3000"])
@@ -10,6 +13,7 @@ CORS(app, support_credentials = True, origins = ["http://localhost3000"])
 
 #Blueprint endpoints
 app.register_blueprint(videoGen_bp, url_prefix = "/videoGen")
+app.register_blueprint(fetchVideo, url_prefix = "/getVideo")
 
 @app.route("/")
 def basics():
