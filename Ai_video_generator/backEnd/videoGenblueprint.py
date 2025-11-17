a1eed07db1d2af2a67a91b521351bf7afbdcfebb
SteffanBurnette <burnettesteffansep7@gmail.com>
2025-11-17 15:56:10 -0500

    Added mongodb upload to the video generation endpoint so that videos are uploaded anbd save as soon as they are created

--- Ai_video_generator/backEnd/videoGenblueprint.py
+++ Ai_video_generator/backEnd/videoGenblueprint.py
@@ -2,6 +2,9 @@ from flask import Blueprint, Flask, request, jsonify
 import time
 from google import genai
 from google.genai import types
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+import gridfs
 import os
 from dotenv import load_dotenv
 
@@ -13,6 +16,15 @@ videoGen_bp = Blueprint("video_generation", __name__)
 load_dotenv("C:/Users/Steffan/Desktop/Ai_advertisement_generator/Ai-Advertisement-Generator/Ai_video_generator/.env")
 API_KEY = os.getenv("GEMINI_API_KEY")
 client = genai.Client(api_key = API_KEY)
+
+#Conencting to the mongoClient
+
+URI = os.getenv("MONGO_CONNECTION_STRING")
+mongo_client = MongoClient(URI, server_api = ServerApi('1'))
+db = mongo_client["test"]
+# Create GridFS bucket
+custom_bucket = gridfs.GridFSBucket(db, bucket_name="myTestBucket")
+print("Loaded URI:", URI)  # DEBUG
 
 #The endpoints
 
@@ -38,6 +50,24 @@ def generate_video():
     generated_video.video.save(f"{data.get("name")}.mp4")
     print(f"Generated video saved to {data.get("name")}.mp4")
 
+    
+    #Uploading to mongo DB
+    # Path to your video
+    video_path = f"{data.get("name")}.mp4"
+    # Make sure file exists
+    print("File exists:", os.path.exists(video_path))  # DEBUG
+
+   # Upload file
+    with open(video_path, "rb") as f:
+        with custom_bucket.open_upload_stream(
+        f'{data.get("name")}.mp4',
+        metadata={"contentType": "video/mp4"}
+         ) as grid_in:
+            grid_in.write(f.read())
+
+    print("Upload complete.")
+
+    
     return jsonify({"Message": f"The video {data.get("name")}.mp4 was successfully created"}), 200
 
 
